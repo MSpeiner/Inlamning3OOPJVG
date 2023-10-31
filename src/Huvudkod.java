d288bac6b40672cf8caa5f99e50af62392f1734b
Filip Detterfelt <fillip.detterfelt@gmail.com>
2023-10-31 11:16:06 +0100

    Skapar upp paneler, knapplista, konstruktor, main metod, samt instans av klassen metod i Huvudkod

--- src/Huvudkod.java
+++ src/Huvudkod.java
@@ -1,3 +1,20 @@
-public class Huvudkod {
+import javax.swing.*;
+import java.util.ArrayList;
 
+public class Huvudkod {
+    //Skapar upp paneler
+    final static JPanel jp = new JPanel();
+    final static JPanel northPanel = new JPanel();
+    final static JPanel centerPanel = new JPanel();
+    final static JPanel southPanel = new JPanel();
+    //SKapar upp instans av Metoder
+    Metoder m = new Metoder(this);
+    //Skapar upp knapplista
+    ArrayList<JButton> knappLista = new ArrayList<>();
+    Huvudkod(){}
+    public static void main(String[] args) {
+
+    }
 }
+
+
